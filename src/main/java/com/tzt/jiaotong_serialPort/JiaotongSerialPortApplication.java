aa6198fc8003837495f502902976f482ba3af37e
tzt <742578852@qq.com>
2021-08-28 14:32:27 +0800

    first commit

---
+++ src/main/java/com/tzt/jiaotong_serialPort/JiaotongSerialPortApplication.java
@@ -0,0 +1,13 @@
+package com.tzt.jiaotong_serialPort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiaotongSerialPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiaotongSerialPortApplication.class, args);
+	}
+
+}
